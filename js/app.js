fd3185b78609956e1900306ab21f077ba71f2764
Claire Carden <smclairecarden@gmail.com>
2022-02-06 09:41:44 -0600

    replay button working

--- js/app.js
+++ js/app.js
@@ -11,10 +11,7 @@
 			[2, 4, 6]
 		]
 
-
 	let boardSquares = []
-
-	
 
 	let T, gameStatus, nextTurn
 
@@ -22,18 +19,8 @@
 		'1' : 'player  X',
 		'-1': 'player O'
 	}
-	
-	
-
-
-	
-
-	console.log(player)
-
 
 /*------------------------ Cached Element References ------------------------*/
-
-
 
 		const gameBoard = document.querySelector(".board")
 
@@ -41,32 +28,34 @@
 
 		const replayBtn = document.querySelector("#button")
 		
-
-
+		const squares = document.querySelectorAll(".game-square")
 
 	// replayBtn.addEventListener("click", function(clear){
 	// gameBoard.children.textContent = ''
 	// })
 	
-gameBoard.addEventListener("click", handleClick)
+		gameBoard.addEventListener("click", handleClick)
 
-gameBoard.addEventListener("reset", init)
+		replayBtn.addEventListener("click", init)
 	
 
 /*-------------------------------- Functions --------------------------------*/
 
-
-
 		function init(){
-			replayBtn.setAttribute("reset", "replay")
+			
+			//gameBoard.setAttribute("reset", render)
 			boardSquares = [null, null, null, null, null, null, null, null, null]
-			message.textContent = `It's ${player ? 'player X' : 'player O'}'s turn!`
+			message.textContent = `It's ${player ? 'Player X' : 'Player O'}'s turn!`
+			render()
+			winner = null;
 			nextTurn = 1
-			isWinner = null;
 			T = 'tie'
+			gameBoard.children.textContent = ''
+			//squares.textContent = ''
 			
 		}	
 		init()
+		
 
 		function render() {
 			switchTurn()
@@ -75,76 +64,63 @@ gameBoard.addEventListener("reset", init)
 			renderWinningMessage()
 			boardSquares.forEach(function(squ, idx) {
 				if (squ === 1) {
-					
-					gameBoard.children[idx].textContent = 'X'
-			
-					
+					gameBoard.children[idx].textContent = 'X'										
 				} else if(squ === -1) {
-					gameBoard.children[idx].textContent = 'O'
-				
-				} 
-			
-			})
-		
+					gameBoard.children[idx].textContent = 'O'					
+				} else if(squ === null) {
+					gameBoard.children[idx].textContent = ''		
+				}	
+			})		
 			}
-			
-		
-		
-	
-
 
 		function switchTurn() {
-			nextTurn *= -1
+				nextTurn *= -1
 		}
 
 		function renderTurn() {
 				if(nextTurn === -1) {
 				return message.textContent = "It's Player O's Turn"
-			} else if(nextTurn === 1){
+				} else if(nextTurn === 1){
 				return message.textContent = "It's Player X's Turn"
-			}
-
+				}
 		}
 
-
 			function handleClick(evt) {
-			const index = (evt.target.id.replace('sq', ''))
-			if(boardSquares[index] === null) {
-
-			boardSquares[index] = nextTurn
-			console.log(boardSquares)
+				const index = (evt.target.id.replace('sq', ''))
+				if(boardSquares[index] === null) {
+				boardSquares[index] = nextTurn
+				console.log(boardSquares)
 				render()
-			} 
+				} 
 			}
 
 			function getWinner() {
 				winningCombos.forEach(combo => {
 				if(Math.abs(boardSquares[combo[0]] + boardSquares[combo[1]] + boardSquares[combo[2]] === 3)) {
-				return isWinner = nextTurn
+				return winner = nextTurn
 				} else if(!boardSquares.includes(null)) {
-					return isWinner = T
+				return winner = T
 				}
 				}) 
 			}
 
+			// function renderWinningMessage() {
+			// 	if(isWinner === -1) {
+			// 	return message.textContent =  `Congrats to the winner, player X!`
+			// 	} else if(isWinner === 1) {
+			// 	return message.textContent =  `Congrats to the winner, player O!`
+			// 	} else if(isWinner === T) {
+			// 	return message.textContent = `It's a tie!`
+			// 	}
+			// }
+
+
 			function renderWinningMessage() {
-				if(isWinner === -1) {
-				return message.textContent =  `Congrats to the winner, player X!`
-				} else if(isWinner === 1) {
-				return message.textContent =  `Congrats to the winner, player O!`
-				} else if(isWinner === T) {
+				if(winner === 1 || winner === -1) {
+				return message.textContent =  `Congrats to the winner, ${player ? player[1] : player[-1]}!`
+				} else if(winner === T) {
 				return message.textContent = `It's a tie!`
 				}
 			}
-
-		let renderTieMessage = function(){
-			if(isWinner === T) {
-			
-			}
-		}
-
-	
-
-
 
 			
